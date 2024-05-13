609d5233a7cd23ffc34567bb67cf1a13d761b24d
Shumokh <154021913+06-shuu@users.noreply.github.com>
2024-05-13 09:18:38 +0300

    change the color when hover over grid cells

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-const grid = document.querySelector(".flexbox-grid");
+const grid = document.querySelector("#sketchpad");
 
 let rows = 16;
 let cols = 16;
-const gridSize = 600;
+const gridSize = 500;
 
 grid.style.width = `${gridSize}px`;
 grid.style.height = `${gridSize}px`;
 
-function createGridBoxes() {
+function createGridBoxes(rows, cols) {
     for (let i = 0; i < (rows * cols); i++) {
-        const gridDiv = document.createElement("div");
-
-        gridDiv.style.width = `${(gridSize / cols) - 2}px`;
-        gridDiv.style.height = `${(gridSize / rows) - 2}px`;
+        let gridDiv = document.createElement("div");
+        // gridDiv.classList.add('gridDiv');
         gridDiv.classList.add("box");
-
+        gridDiv.style.width = `${(gridSize / cols)}px`;
+        gridDiv.style.height = `${(gridSize / rows)}px`;
         grid.appendChild(gridDiv);
+
+        gridDiv.addEventListener("mouseenter", () =>{
+            gridDiv.style.backgroundColor = "black";
+        })//
     }
 };
 
-createGridBoxes();
+createGridBoxes(rows, cols);
+
+
+// function try11(){
+//     const t = document.createElement("p");
+//     t.innerHTML=`hi`
+//     grid.appendChild(t);
+// };
+
+// try11()
